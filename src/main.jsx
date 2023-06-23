19a7d361425de42e45fd8b46950b2eb0bf3cbcc5
Mohammad Arkan <a.r.khan.coxyboys@gmail.com>
2023-06-23 19:02:01 +0600

    implement gradient background color

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         element:<Statistics/>
       },
       {
-        path:"/appliedjobs",
+        path:"/applied",
         element:<AppliedJobs/>
       },
       {
