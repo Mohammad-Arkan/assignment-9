d88db7cd759d72eb1b7fe1971fca8267e8e14b35
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-23 10:35:52 +0600

    added components

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home.jsx';
+import Statistics from './components/Statistics.JSX';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path:"/statistics",
+        element:<Statistics/>
+      },
+      {
+        path:"/blog",
+        element:<Blob/>
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
