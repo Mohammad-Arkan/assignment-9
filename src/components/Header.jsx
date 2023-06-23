eafe3a0c21f232aacfa25b5071cb70ea2f88b13e
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-23 10:43:03 +0600

    addde nav items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h2>HEADER</h2>
+            <div>
+                <div>JobWorld</div>
+                <div>
+                    <ul>
+                        <li>
+                            <NavLink to="/" 
+                              className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" 
+                              className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >Statistics</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" 
+                              className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >Applied Job</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" 
+                              className={({ isActive }) => (isActive ? 'active' : 'default')}
+                            >Blog</NavLink>
+                        </li>
+                       
+                    </ul>
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
